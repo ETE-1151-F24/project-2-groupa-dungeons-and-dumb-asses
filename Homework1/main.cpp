df34d4fab2db93a255586b7e212667609b9c8f4b
hells-scholar-ie-me <echolmes@cpp.edu>
2024-08-30 18:06:19 -0700

    well it ran

--- Homework1/main.cpp
+++ Homework1/main.cpp
@@ -1,9 +1,14 @@
-int main(){
 
-// code goes below here
-//i did a thing
-//code goes above here 
 
-return 0;                       
+#include<iostream>
 
+int main() {
+
+    // code goes below here
+    std::cout << "Hello, World!" << std::endl;
+    // I did a thing
+    std::cout << "I did a thing!" << std::endl;
+    // code goes above here
+
+    return 0;                       
 }
