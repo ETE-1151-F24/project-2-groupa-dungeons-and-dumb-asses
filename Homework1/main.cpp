15f059c3bc0e2928e08eaff8cded1f318b240ef9
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-01 18:12:31 -0700

    Update main.cpp

--- Homework1/main.cpp
+++ Homework1/main.cpp
@@ -1,9 +1,12 @@
-int main(){
+#include<iostream>
 
-// code goes below here
-//i did a thing
-//code goes above here 
+int main() {
 
-return 0;                       
+    // code goes below here
+    std::cout << "Hello, World!" << std::endl;
+    // I did a thing
+    std::cout << "I did a thing!" << std::endl;
+    // code goes above here
 
+    return 0;                       
 }
