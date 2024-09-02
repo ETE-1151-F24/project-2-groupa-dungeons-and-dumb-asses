dbabfd437679cf436fd44c488b62ae9439b8f427
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-01 18:47:48 -0700

    initial commit

---
+++ Homework2/functions.h
@@ -0,0 +1,9 @@
+// this is where i put my functions
+// Using the information you have learned so far (and a little google foo):
+
+// Create a program that takes and input and converts it to binary 
+// Program should be able to return correct binary value of Chars, int and floats
+// +1 point for creating a function
+// Here is a helpful web page
+// function for character
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+
+    // code goes below here
+   
+    // I did a thing
+    //std::cout << "I did a thing!" << std::endl;
+    // code goes above here
+    
+//if thing is character the convert char to binary
+//if thing is integer then convert int to binary
+// if thing is float, then convert float to binary
+
+
+
+
+    return 0;                       
+}
