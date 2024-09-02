060c54ef717ac2da72ac47e43ed1527522580449
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-02 12:54:50 -0700

    starting to put code in places

--- Homework2/functions.h
+++ Homework2/functions.h
@@ -8,5 +8,20 @@
 // function for character
 
 //function for converting charachter to binary
+
+// int addNumbers( int first_param, int second_param){
+//     int result = first_param + second_param;
+//     return result;
+// }       
+
+int charToBinary(char){
+    //convert charachter to binary
+    //int resultChar = 
+}
+
+
 //function for converting integer to binary
+
+
+
 //function for converting floats to binary
