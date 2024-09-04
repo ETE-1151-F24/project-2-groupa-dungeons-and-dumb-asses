61f402a79122ff688ac79c131ceb25ec3b0dccb5
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-04 14:16:56 -0700

    looking up stack overflow code

--- main.cpp
+++ main.cpp
@@ -1,30 +1,23 @@
-#include "conversionStuff.h"
+#include "conversionStuff.h" // this includes a reference to the content of the file [conversionStuff.h]
 #include<iostream>
 #include<string>
 
-using namespace std;
+using namespace std; //this makes it the std:: naming scheme so i dont have to type it over and over
 
+void charToBinary(char c); //initializes the char to binary function and references variable [c]
+void numToBinary(int n);  //initializes the number to binary function and references variable [n]
+void floaterToBinary(float f);  //initializes the floater to binary function and references variable [f]
 
 
 int main() {
     //this section of code might be best suited as an intro function
-
-string inputToConvert; //no initializer has default initialization
-stdcout << "enter input";
-cin<< inputToConvert
-
+    string inputToConvert; // this is intitializing a string to store an input in a variable named [inputToConvert]
+    cout << "Input something to convert to binary" << endl; // this the output prompt message at the beginning of the program
+    cin >> inputToConvert;
 
 
+//this is where i am checking to see what kind of input it is that i am working with
 
-charToBinary(); //initializes the char to binary function
-numToBinary();  //initializes the number to binary function
-floaterToBinary();  //initializes the floater to binary function
-
-
-//this is where i would make an input statement and code to initialize input
-std :: cout << "input to convert" << input << std :: endl;
-
-        //this is where i would question the type of input and ask if it is chars, int, or float
 
 
 //if thing is character then convert char to binary
