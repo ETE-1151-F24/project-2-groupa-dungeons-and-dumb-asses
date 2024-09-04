efa85b622ffeddd280131475e3e4d4d204fad03f
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-04 16:07:47 -0700

    charToBinary code inputted in conversionstuff.cpp

---
+++ Homework2/conversionStuff.cpp
@@ -0,0 +1,12 @@
+//here is where i have my functions stored as far as what each one will do
+//i had to use syntax pulled from stackoverflow in order to perform the operations because i didnt know what the correct syntax was
+
+//here is the code for the function [charToBinary]
+
+
+//here is the code for the function [numToBinary]
+
+
+
+
+//here is the code for the function [floaterToBinary]
