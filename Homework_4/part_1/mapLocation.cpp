f3a43cc7b3897b527860b7ee723af69fb27211fd
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-09 15:29:24 -0700

    ******Homework 4 started

---
+++ Homework_4/part_1/mapLocation.cpp
@@ -0,0 +1,26 @@
+// Part 1: easy.
+// Write a C++ program that prompts the user to enter their name and location point (x, y) on a map.
+
+// The program will check whether the point is within the a random sized, map width of 20 and height 20 centered at 0,0 ie a(-10,-10),b(-10,10),c(10,10), d(10,-10).
+
+// Hard requirements
+
+// Use a function to check if the location is valid.
+// Use a function to create the random sized map.
+// Use if else.
+// Have fun with this 
+// Soft requirements (optional)
+
+// Use a global variable
+// Use logical operators.
+// Do not have fun with this (this option will override the hard requirement, and you will be issued a -1 for your enjoyment)
+ 
+
+#include <cstdlib> // Required for rand()
+#include <iostream>
+int main() {
+  std::cout << rand() % 100 << " "; // Numbers between 0 and 99
+  return 0;
+}
+
+
---
+++ Homework_4/part_2/yearCalendar.cpp
@@ -0,0 +1,34 @@
+// Part 2: hard.
+// Write a program that prompts the user for a year(like 1900 or 2450) then print the year calendar. This program is slightly challenging and may take you a while to get right. 
+
+// You will need to read up on how to use iomanip to format your output.
+
+// Use Case
+// Use for 
+// Use if
+
+// January = 31;
+// February 29 / 28 Check for Leap years
+// March = 31
+// April= 30
+// May= 31
+// June = 30;
+// July  = 31;
+// August= 31;
+// September = 30;
+// October = 31;
+// November = 30;
+// December  = 31;
+
+// Example Output
+
+// Enter a year  :  2024
+
+// Calendar for 2024
+// --January 2024 --
+//  Mon     Tue     Wed     Thu     Fri     Sat     Sun
+//  1       2       3       4       5       6       7
+//  8       9       10      11      12      13      14
+//  15      16      17      18      19      20      21
+//  22      23      24      25      26      27      28
+//  29      30      31
