a989bdefea1d4e78beae004430919efd629cb9af
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-17 15:09:48 -0700

    reorganized function structure in function implementation file

--- Homework_4/part_2/calendarCalcFunctions.cpp
+++ Homework_4/part_2/calendarCalcFunctions.cpp
@@ -3,20 +3,78 @@
 #include <iostream>                     // For input/output functionality
 
 
+//                                                          [getYear]
+//--------------------------------------------FUNCTION TO GET USER INPUT FOR YEAR-----------------------------
+int getYear() {                                                                              //want function to not accept years before 1900
+     int year;                                                                                      // Declare variable to store the input year
 
-//--------------------------------------------FUNCTION TO CHECK FOR A LEAP YEAR-----------------------------        
-//leap year if either condition is satisfied:                                   //1. The year is multiple of 400.
-                                                                                
-                                                                                //2. The year is multiple of 4 (year % 4 == 0)...
-                                                                                //   and (&&) not multiple of 100 (year % 100 !=0).
-bool isLeapYear(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    while (true) {
+        std::cout << std::endl <<"Enter a year (>= 1900): ";                                                    // Prompt the user for a year
+        std::cin >> year;
+                                                                                                    //USING ERROR HANDLING FROM MAP PROGRAM
+        if (std::cin.fail()) {                                                                      // Check if input is valid
+            std::cout << std::endl << "You may Whisper sweet nothings to me..." << std::endl;
+            std::cout << " but Please make your sweet nothings a VALID NUMBER." << std::endl;
+            std::cin.clear();                                                                       // Clear error flag on input stream
+            std::cin.ignore(1000, '\n');                                                            // Clear any remaining input in the stream
+        } else if(year < 1900){
+            std::cout << std::endl << "yeah... THAT'S LIKE A waaay LONG TIME AGO," << std::endl;
+            std::cout << "YOU SHOULD JUST START FROM LIKE 1900 AND GOING FORWARD" << std::endl;
+        } else {
+            return year;
+        }
+    }
 }
 //----------------------------------------------------------------------------------------------------------
 
 
 
 
+//                                                         [getMonth]
+//--------------------------------------------FUNCTION TO GET USER INPUT FOR MONTH-----------------------------
+int getMonth() {                                                                                        // Function to get user input for month
+    int month;                                                                                         // Declare variable to store the input month
+
+    while (true) {
+        // Display the options for months
+        std::cout << std::endl <<"Choose a month by entering the corresponding number:\n";
+        std::cout << "[ 1: Jan], [ 2: Feb], [ 3: Mar], [ 4: Apr], [ 5: May], [ 6: Jun]\n";
+        std::cout << "[ 7: Jul], [ 8: Aug], [ 9: Sep], [10: Oct], [11: Nov], [12: Dec]\n";
+         
+        std::cout << std::endl <<"Choose a month (1-12): ";                                                           // Prompt the user for a month
+        std::cin >> month;
+
+        // Error handling: Check if input is not a valid number
+        if (std::cin.fail()) {  
+            std::cout << std::endl <<"I'm sorry, I didn't quite get that." << std::endl;
+            std::cout << "Please make your input a 'VALID' NUMBER." << std::endl;
+            std::cin.clear();  // Clear error flag on input stream
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Clear any remaining input
+        } else if (month < 1 || month > 12) {  // Check if input is outside valid range (1-12)
+            std::cout << std::endl <<"*facepalm* Please choose a 'VALID' NUMBER (1-12)." << std::endl;
+        } else {
+            return month;  // Return the valid month input
+        }
+    }
+}
+//----------------------------------------------------------------------------------------------------------
+
+
+
+//                                                        [isLeapYear]
+//--------------------------------------------FUNCTION TO CHECK FOR A LEAP YEAR-----------------------------        
+                                                                                //leap year if either condition is satisfied:                                   
+                                                                                //1. The year is multiple of 400.
+                                                                                //2. The year is multiple of 4 (year % 4 == 0)...
+                                                                                //   and (&&) not multiple of 100 (year % 100 !=0).
+bool isLeapYear(int year) {
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}                                                                               //function needed for evaluating [findMonthLength]
+//----------------------------------------------------------------------------------------------------------
+
+
+
+//                                                      [findMonthLength]
 //--------------------------------------------FUNCTION TO CHECK DAYS IN THE MONTH-----------------------------
 int findMonthLength(int year, int month) {
     //switch operator will go here
@@ -40,7 +98,7 @@ int findMonthLength(int year, int month) {
 //----------------------------------------------------------------------------------------------------------
 
 
-
+//                                                          [printMonth]
 //--------------------------------------------FUNCTION TO PRINT THE DAYS OF THE MONTH-----------------------------
 void printMonth(int year, int month, int &startWeekDay) {                       //int [&startDay], initializes the variable [startDay] 
                                                                             //and references it so the print function can modify it
@@ -79,64 +137,7 @@ std::cout << std::endl <<"OOOOOHhh!...look at it....LOOK...AT...IT!" << std::end
 //----------------------------------------------------------------------------------------------------------
 
 
-
-
-
-//--------------------------------------------FUNCTION TO GET USER INPUT FOR YEAR-----------------------------
-int getYear() {                                                                              //want function to not accept years before 1900
-     int year;                                                                                      // Declare variable to store the input year
-
-    while (true) {
-        std::cout << std::endl <<"Enter a year (>= 1900): ";                                                    // Prompt the user for a year
-        std::cin >> year;
-                                                                                                    //USING ERROR HANDLING FROM MAP PROGRAM
-        if (std::cin.fail()) {                                                                      // Check if input is valid
-            std::cout << std::endl << "You may Whisper sweet nothings to me..." << std::endl;
-            std::cout << " but Please make your sweet nothings a VALID NUMBER." << std::endl;
-            std::cin.clear();                                                                       // Clear error flag on input stream
-            std::cin.ignore(1000, '\n');                                                            // Clear any remaining input in the stream
-        } else if(year < 1900){
-            std::cout << std::endl << "yeah... THAT'S LIKE A waaay LONG TIME AGO," << std::endl;
-            std::cout << "YOU SHOULD JUST START FROM LIKE 1900 AND GOING FORWARD" << std::endl;
-        } else {
-            return year;
-        }
-    }
-}
-//----------------------------------------------------------------------------------------------------------
-
-
-
-//--------------------------------------------FUNCTION TO GET USER INPUT FOR MONTH-----------------------------
-int getMonth() {                                                                                        // Function to get user input for month
-    int month;                                                                                         // Declare variable to store the input month
-
-    while (true) {
-        // Display the options for months
-        std::cout << std::endl <<"Choose a month by entering the corresponding number:\n";
-        std::cout << "[ 1: Jan], [ 2: Feb], [ 3: Mar], [ 4: Apr], [ 5: May], [ 6: Jun]\n";
-        std::cout << "[ 7: Jul], [ 8: Aug], [ 9: Sep], [10: Oct], [11: Nov], [12: Dec]\n";
-         
-        std::cout << std::endl <<"Choose a month (1-12): ";                                                           // Prompt the user for a month
-        std::cin >> month;
-
-        // Error handling: Check if input is not a valid number
-        if (std::cin.fail()) {  
-            std::cout << std::endl <<"I'm sorry, I didn't quite get that." << std::endl;
-            std::cout << "Please make your input a 'VALID' NUMBER." << std::endl;
-            std::cin.clear();  // Clear error flag on input stream
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Clear any remaining input
-        } else if (month < 1 || month > 12) {  // Check if input is outside valid range (1-12)
-            std::cout << std::endl <<"*facepalm* Please choose a 'VALID' NUMBER (1-12)." << std::endl;
-        } else {
-            return month;  // Return the valid month input
-        }
-    }
-}
-//----------------------------------------------------------------------------------------------------------
-
-
-
+//                                                         [calculateStartDay]
 //--------------------------------------------FUNCTION TO FIND START DAY OF A MONTH BASED ON THE YEAR-----------------------------
 // THE YEAR 1900 BEGAN ON A MONDAY
 // ANY YEAR BEFORE 1900, DISPLAY AN ERROR MESSAGE 
