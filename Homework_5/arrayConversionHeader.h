3d2c3a2d4f6279c62b4e58682d41d32e2b4db203
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-21 16:59:04 -0700

    setting up character checking and quantity updating

---
+++ Homework_5/arrayConversionHeader.h
@@ -0,0 +1,29 @@
+#ifndef ARRAYCONVERSIONHEADER_H //CALENDAR_H // Include guard to prevent multiple definitions
+#define ARRAYCONVERSIONHEADER_H
+
+//-------------------INCLUDES-------------------------------
+
+#include <iostream>
+#include <iomanip>   // [iomanip] allows for formatting of output and alignment
+#include <string>
+#include <cstring>
+
+//-------------------Declaring global variables-----------------
+std::string userInputString; 
+
+
+
+
+
+//-------------------Preliminary initialize of functions-----------------
+void wordSaladGreeting();
+void getArrayInput();
+void stringToArrayConversion();
+void checkCharacterType();
+void displayCharTypes();
+void closingMessageExit();
+
+
+
+
+#endif // End of include guard
--- Homework_5/arrayFunctions.cpp
+++ Homework_5/arrayFunctions.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<cstring>            //include the cstring library contains various functions for working with C-style strings 
+#include "arrayConversionHeader.h"
+#include <limits>  // For std::numeric_limits such as 
 
 // -------------------------FUNCTION FOR PROGRAM GREETING
 void wordSaladGreeting(){
@@ -23,6 +25,8 @@ std::string userInputString;             // Declare a string variable 's' to hol
 
     std::cout << "Are you ready to get that Wordsalad tossed?"<< std::endl;    // Prompt to press any key to continue
     std::cout << "Press any key to continue..." << std::endl;
+        // Clear the input buffer to ignore any leftover newline characters
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');//  need [#include <limits>]  For std::numeric_limits
     std::cin.get();                                                             // Wait for a single character input
 
 
@@ -31,6 +35,9 @@ std::string userInputString;             // Declare a string variable 's' to hol
 
 void stringToArrayConversion(){
 
+char* characterStorageArray = new char[userInputString.length() + 1];  // +1 for the null terminator
+std::strcpy(characterStorageArray, userInputString.c_str());           // Copy std::string to char array
+
 
 }
 
@@ -38,6 +45,14 @@ void stringToArrayConversion(){
 // -------------------------FUNCTION TO EVALUATE THE CHARACTER QUANTITIES
 
 void checkCharacterType(){
+    int spaces=0;
+    int vowels=0;
+    int consonants=0;
+    int numericDigits=0;
+    int spacesOrTabs=0;
+
+// here is the for statement to loop through [characterStorageArray] and determine the type of charachter in the array
+
 //std::isspace() //checks whether a character is a whitespace character, including spaces, tabs, newlines, etc
 //std::isalpha(): Checks if a character is alphabetic.
 //isVowel(): Called within the if block to check if the alphabetic character is a vowel or consonant.
