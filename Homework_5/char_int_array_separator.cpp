af3e8b1bfb8ec8832abfc0d50d783d6372c30adc
hells-scholar-ie-me <echolmes@cpp.edu>
2024-09-20 18:08:27 -0700

    ----Homework #5 conceptual flow of the program

---
+++ Homework_5/char_int_array_separator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>            // Includes the standard input/output stream library for cout
+#include <limits>                       // For numeric limits to handle input errors
+
+int main() {
+// i need to declare an array that will first be able to store the input stream
+// also have a prompt to say no more that a certain amount of characters
+// error message if too many are entered.
+// loop it back to entry to try again
+//convert the input string into individual items in the array
+// for caracter in wordinput, make the next index of the array that value.  
+// then look throught the array and  perform
+//initialize [int charCount = 0] variable
+//iniotialize [int numericCount = 0] variable
+// for item in declared array.....if char then charCount +=1
+//                              else if numeric,then numericCount +=1
+
+//while indexPosition is < max lenght of the array keep going with the counting
+//  once done output a message
+/*  "in [inputString] there are [charCount] letters and [numericCount] numbers "
+
+then have an output message "thank you and have a nice dice my wordsalad tosser"
+
+"press any key to exit" message
+clear input buffer of stream
+wait for user to press any key to exit
+
+
+*/
+int generic;
+
+std::cout << "" << std::endl;
+std::cin >> generic ;
+
+    return 0;
+}
+
